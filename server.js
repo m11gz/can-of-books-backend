e6633d42df77caccd0aea8c87d73b451449b56d3
Miguel Rodriguez <7migro11@gmail.com>
2022-08-29 15:56:14 -0700

    port

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
-
-  response.send('test request received')
-
-})
+app.get("/test", (request, response) => {
+  response.send("test request received");
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+// ROUTES
+app.get("/", (request, response) => {
+  response.status(200).send("Welcome!");
+});
+
+app.get("*", (request, response) => {
+  response.status(404).send("Not available");
+});
+
+//ERROR
+app.use((error, request, response, next) => {
+  response.status(500).send(error.message);
+});
+
+// LISTEN
+app.listen(PORT, () => console.log("listening on Port ${PORT}"));
