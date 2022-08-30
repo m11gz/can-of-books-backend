668273b4178158bf096d59dadd19294a6314333d
DAVE.TUSIA@GMAIL.COM~ <DAVE.TUSIA@GMAIL.COM>
2022-08-29 21:35:19 -0700

    Pushing updates to github. Code not finished

--- models/books.js
+++ models/books.js
@@ -8,15 +8,14 @@ const { Schema } = mongoose;
 
 // create a cat schema, define how our object will be structured
 const BookSchema = new Schema({
-  name: {type: String, required: true},
-  genre: {type: String, required: true},
-  status: {type: Boolean, required: true},
-  location: {type: String, required: true}
+  title: {type: String, required: true},
+  description: {type: String, required: true},
+  status: {type: String, required: true},
 });
 
 // define the model
 // it gives mongoose functionality and a predefined schema to shape our data
 // it takes in a string and a scheme
-const CatModel = mongoose.model('Book', BookSchema);
+const BookModel = mongoose.model('Book', BookSchema);
 
-module.exports = CatModel;
+module.exports = BookModel;
